ab2206b7e6a12c7d6b4aecc0802d6a6e55ef3a11
maniek <mpastuszka@gmail.com>
2018-09-23 15:29:18 +0200

    [hotfix] - fixing PaymentMethod class

--- src/main/java/pl/mpas/homebudget/domain/PaymentMethod.java
+++ src/main/java/pl/mpas/homebudget/domain/PaymentMethod.java
@@ -1,9 +1,20 @@
 package pl.mpas.homebudget.domain;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum PaymentMethod {
 
     CASH,
     DEBIT,
     CREDIT,
-    ONLINE_PAYMENT
+    ONLINE_PAYMENT;
+
+    public static List<PaymentMethod> getAllPaymentMethods() {
+        return Arrays.stream(PaymentMethod.values())
+                .sorted(Comparator.comparing(Enum::toString))
+                .collect(Collectors.toList());
+    }
 }
